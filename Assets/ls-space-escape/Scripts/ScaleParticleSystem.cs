9a6450823b02b298d3d899df8fddb855ba699de8
lusfikars <lusfikars@gmail.com>
2018-02-08 08:29:50 +0700

    release 1.4

--- Assets/ls-space-escape/Scripts/ScaleParticleSystem.cs
+++ Assets/ls-space-escape/Scripts/ScaleParticleSystem.cs
@@ -10,7 +10,48 @@ namespace SpaceEscape
 
         private void Start()
         {
-            ParticleScaler.Scale(GetComponent<ParticleSystem>(), scale, true);
+            Scale(GetComponent<ParticleSystem>(), scale, true);
+        }
+
+        public void Scale(ParticleSystem particleSystem, float scale, bool scaleChildren)
+        {
+            if (!particleSystem)
+                return;
+
+            DoScale(particleSystem, scale, false);
+
+            if (scaleChildren)
+            {
+                ParticleSystem[] children = particleSystem.GetComponentsInChildren<ParticleSystem>();
+
+                for (int i = children.Length; i-- > 0;)
+                {
+                    if (children[i] == particleSystem)
+                    {
+                        continue;
+                    }
+
+                    DoScale(children[i], scale, true);
+                }
+            }
+        }
+
+        private void DoScale(ParticleSystem particleSystem, float scale, bool scalePos)
+        {
+            if (scalePos)
+            {
+                particleSystem.transform.localPosition *= scale;
+            }
+
+            ParticleSystem.MainModule psMain = particleSystem.main;
+
+            psMain.startSizeMultiplier *= scale;
+            psMain.gravityModifierMultiplier *= scale;
+            psMain.startSpeedMultiplier *= scale;
+
+            ParticleSystem.ShapeModule shape = particleSystem.shape;
+            shape.radius *= scale;
+            shape.scale = shape.scale * scale;
         }
     }
 }
